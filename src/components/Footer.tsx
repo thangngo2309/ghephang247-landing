d6c8ec98b0c45766b956c48dbdf6778bf1f450e4
Thang Ngo <hoangthang23091993@gmail.com>
2026-04-02 08:29:35 +0700

    add hotline

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -6,6 +6,7 @@ export default function Footer() {
         <div className="max-w-6xl mx-auto px-6 md:px-0 text-center space-y-2">
           <h4 className="text-lg font-semibold text-white">CÔNG TY TNHH GIẢI PHÁP CÔNG NGHỆ HOÀNG ÂN</h4>
           <p>Địa chỉ: Đà Nẵng - Việt Nam</p>
+          <p>Hotline: 19005361</p>
           <p>Email: contact@gpcnhoangan.com</p>
           <p className="text-sm text-gray-500 mt-4">© 2025 GhepHang247. All rights reserved.</p>
         </div>
